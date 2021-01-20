78033cf6af4c6692bf45be8ce630d603e76fad0f
Ariel <a.leiradk@gmail.com>
2021-01-20 10:43:56 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,12 @@
+"""App Server main"""
+# importing dependencies or modules
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_cors import CORS
+
+# importing resources
+
+
+# Run the main app
+if __name__ == '__main__':
+    app.run(debug=True)
